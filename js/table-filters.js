098e2d63e4c5578c0bca59ab68b729cc6fb874ca
Novater <cookiesnmilkjk@gmail.com>
2022-01-16 19:24:47 -0500

    Fix Table URIs

--- js/table-filters.js
+++ js/table-filters.js
@@ -2,13 +2,13 @@ const TABLE_TO_URI_MAP = {
   "abyss-table":
     "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=033af0b1-d082-4238-83be-3b1224b96c81&maxDataAge=3600&theme=light&autoRefresh=true&filter=",
   "domain-table":
-    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=4281f477-53cd-428c-b366-71eb74edd98a&amp;maxDataAge=3600&amp;theme=light&amp;autoRefresh=true",
+    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=4281f477-53cd-428c-b366-71eb74edd98a&maxDataAge=3600&theme=light&autoRefresh=true&filter=",
   "event-table":
-    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=3dfb74bf-d908-4d69-90a6-764bd1ee7c5f&amp;maxDataAge=3600&amp;theme=light&amp;autoRefresh=true",
+    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=3dfb74bf-d908-4d69-90a6-764bd1ee7c5f&maxDataAge=3600&theme=light&autoRefresh=true&filter=",
   "weekly-boss-table":
-    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=5952c1ae-0664-43d1-a818-e4a907acb6a2&amp;maxDataAge=3600&amp;theme=light&amp;autoRefresh=true",
+    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=5952c1ae-0664-43d1-a818-e4a907acb6a2&maxDataAge=3600&theme=light&autoRefresh=true&filter=",
   "world-boss-table":
-    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=6ef76005-c2ef-4264-bc68-011b2877e74c&amp;maxDataAge=3600&amp;theme=light&amp;autoRefresh=true",
+    "https://charts.mongodb.com/charts-project-0-bufgk/embed/charts?id=6ef76005-c2ef-4264-bc68-011b2877e74c&maxDataAge=3600&theme=light&autoRefresh=true&filter=",
 };
 
 const TABLE_TO_FILTERS = {
